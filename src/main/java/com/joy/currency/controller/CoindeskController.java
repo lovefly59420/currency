c7b48b7fb48c90467ba6a39b35f8e4b4377202c1
lovefly59420 <lovefly59420@gmail.com>
2025-01-06 14:28:07 +0800

    update API description

--- src/main/java/com/joy/currency/controller/CoindeskController.java
+++ src/main/java/com/joy/currency/controller/CoindeskController.java
@@ -5,6 +5,10 @@ import com.joy.currency.dto.ServiceNameEnum;
 import com.joy.currency.service.factory.AbstractFactory;
 import com.joy.currency.service.interfac.ICoindeskService;
 import com.joy.currency.util.AESUtil;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/coindesk")
+@Tag(name = "coindesk 相關服務", description="查詢、轉換資料")
 public class CoindeskController {
     @Autowired
     private AbstractFactory coindeskServiceFactory;
 
     private AESUtil aesUtil = AESUtil.getInstance();
 
+    @Operation(summary = "取得 coindesk API 資料")
     @GetMapping("/get/data")
     public ResponseEntity<String> getData() throws Exception {
         ICoindeskService service = coindeskServiceFactory.getICoindeskService(ServiceNameEnum.COINDESK_SERVICE.getName());
@@ -26,6 +32,7 @@ public class CoindeskController {
         return ResponseEntity.status(response.getStatusCode()).body(aesUtil.encrypt(response));
     }
 
+    @Operation(summary = "將 coindesk API 資料轉換成特定格式")
     @GetMapping("/transform/data")
     public ResponseEntity<String> transformData() throws Exception {
         ICoindeskService service = coindeskServiceFactory.getICoindeskService(ServiceNameEnum.COINDESK_SERVICE.getName());
--- src/main/java/com/joy/currency/controller/CurrencyCategoryController.java
+++ src/main/java/com/joy/currency/controller/CurrencyCategoryController.java
@@ -3,10 +3,14 @@ package com.joy.currency.controller;
 import com.joy.currency.dto.Response;
 import com.joy.currency.dto.ServiceNameEnum;
 import com.joy.currency.entity.CurrencyCategory;
-import com.joy.currency.exception.CurrencyException;
 import com.joy.currency.service.factory.AbstractFactory;
 import com.joy.currency.service.interfac.ICurrencyCategoryService;
 import com.joy.currency.util.AESUtil;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/currency")
 @RequiredArgsConstructor
+@Tag(name = "幣別管理", description="新增、刪除、查詢、改正")
+@OpenAPIDefinition(info = @Info(title = "幣別管理文件", version = "0.0.1"))
 public class CurrencyCategoryController {
 
     @Autowired
@@ -24,6 +30,9 @@ public class CurrencyCategoryController {
 
     private AESUtil aesUtil = AESUtil.getInstance();
 
+    @Operation(summary = "新增一筆幣別資料", description = "只能新增一筆")
+    @ApiResponse(responseCode = "200", description = "成功新增幣別資料")
+    @ApiResponse(responseCode = "500", description = "失敗")
     @PostMapping("/add")
     public ResponseEntity<String> add(@RequestBody String encryptedCurrencyCategory) throws Exception {
         List<CurrencyCategory> saveData = aesUtil.decrypt(encryptedCurrencyCategory);
@@ -32,6 +41,7 @@ public class CurrencyCategoryController {
         return ResponseEntity.status(response.getStatusCode()).body(aesUtil.encrypt(response));
     }
 
+    @Operation(summary = "新增多筆幣別資料", description = "可新增多筆")
     @PostMapping("/add/all")
     public ResponseEntity<String> addAll(@RequestBody String encryptedCurrencyCategory) throws Exception {
         List<CurrencyCategory> saveData = aesUtil.decrypt(encryptedCurrencyCategory);
@@ -40,6 +50,7 @@ public class CurrencyCategoryController {
         return ResponseEntity.status(response.getStatusCode()).body(aesUtil.encrypt(response));
     }
 
+    @Operation(summary = "更新幣別資料", description = "使用幣別代碼更新幣別中文名稱")
     @PutMapping("update")
     public ResponseEntity<String> update(@RequestParam(value = "currency", required = true) String currency,
                                            @RequestParam(value = "currencyChineseName", required = true) String currencyChineseName) throws Exception {
@@ -48,6 +59,7 @@ public class CurrencyCategoryController {
         return ResponseEntity.status(response.getStatusCode()).body(aesUtil.encrypt(response));
     }
 
+    @Operation(summary = "取得所有幣別資料")
     @GetMapping("find/all")
     public ResponseEntity<String> findAll(@RequestParam(value = "lang", required = false) String lang) throws Exception {
         ICurrencyCategoryService service = currencyCategoryServiceFactory.getICurrencyCategoryService(ServiceNameEnum.CURRENCY_CATEGORY_SERVICE.getName());
@@ -55,6 +67,7 @@ public class CurrencyCategoryController {
         return ResponseEntity.status(200).body(aesUtil.encrypt(response));
     }
 
+    @Operation(summary = "取得特定幣別資料", description = "使用幣別代碼取得幣別資料")
     @GetMapping("/find/{currency}")
     public ResponseEntity<String> findByCurrency(@PathVariable String currency, @RequestParam(value = "lang", required = false) String lang) throws Exception {
         ICurrencyCategoryService service = currencyCategoryServiceFactory.getICurrencyCategoryService(ServiceNameEnum.CURRENCY_CATEGORY_SERVICE.getName());
@@ -62,6 +75,7 @@ public class CurrencyCategoryController {
         return ResponseEntity.status(response.getStatusCode()).body(aesUtil.encrypt(response));
     }
 
+    @Operation(summary = "刪除特定幣別資料", description = "使用幣別代碼刪除幣別資料")
     @DeleteMapping("/delete/{currency}")
     public ResponseEntity<String> delete(@PathVariable String currency) throws Exception {
         ICurrencyCategoryService service = currencyCategoryServiceFactory.getICurrencyCategoryService(ServiceNameEnum.CURRENCY_CATEGORY_SERVICE.getName());
@@ -69,6 +83,7 @@ public class CurrencyCategoryController {
         return ResponseEntity.status(response.getStatusCode()).body(aesUtil.encrypt(response));
     }
 
+    @Operation(summary = "刪除所有幣別資料")
     @DeleteMapping("/delete/all")
     public ResponseEntity<String> deleteAll() throws Exception {
         ICurrencyCategoryService service = currencyCategoryServiceFactory.getICurrencyCategoryService(ServiceNameEnum.CURRENCY_CATEGORY_SERVICE.getName());
